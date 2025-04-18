efc83fef9e907189eab996a7a860c669f8d8c169
Himanshi <himanshi.shende619@gmail.com>
2025-04-19 00:52:21 +0530

    Created Weekview page

--- components/ui/week-view.tsx
+++ components/ui/week-view.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { getWeekDays } from '@/lib/getTime'
+import { useDateStore } from '@/lib/store'
 
 export default function WeekView() {
+
+  const {userSelectedDate} = useDateStore()
+
+  const Days = getWeekDays(userSelectedDate)
+
+  console.log(Days)
+
+  
+
+
   return (
     <div>
       
